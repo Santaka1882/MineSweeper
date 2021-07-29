a584a9d1e566b18246e5e385533ac9b559e79506
Cristian Buitrago <Crisis1882@gmail.com>
2021-07-29 16:24:01 -0500

    changes in the render of the gameboard

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
-function Cell(props) {
+// This cell component returns a button that represents
+// one cell in the game
+
+function HiddenCell(props) {
   return (
     <button 
       className='square'
@@ -14,16 +17,131 @@ function Cell(props) {
   )
 }
 
-class Board extends React.Component {
+function BombCell(props) {
+  return(
+    <button 
+      className='square'
+      onClick={props.onClick}
+    >
+      {props.col}
+      {props.row}
+    </button>
+  )
+}
+
+function FlagCell(props) {
+  return (
+    <button 
+      className='square'
+      onClick={props.onClick}
+    >
+      {props.col}
+      {props.row}
+    </button>
+  )
+}
+
+function NumberCell(props) {
+  return (
+    <button 
+      className='square'
+      onClick={props.onClick}
+    >
+      {props.col}
+      {props.row}
+    </button>
+  )
+}
+
+function EmptyCell(props) {
+  return (
+    <button 
+      className='square'
+      onClick={props.onClick}
+    >
+      {props.col}
+      {props.row}
+    </button>
+  )
+}
+
+class Cell extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      board: [],
+      isHidden: true,
+      isBomb: false,
+      isFlag: false,
+      isNumber: false,
+      isEmpty: false
+    }
+  }
+  render () {
+    let cell;
+    if(this.state.isHidden) {
+      cell = <HiddenCell/>
     } 
+
+    if(this.state.isBomb) {
+      cell = <BombCell/>
+    }
+    
+    if(this.state.isFlag) {
+      cell = <FlagCell/>
+    }
+
+    if(this.state.isNumber) {
+      cell = <NumberCell/>
+    }
+
+    if(this.state.isEmpty) {
+      cell = <EmptyCell/>
+    }
+
+    return (
+      cell
+    )
+  }
+}
+
+//The board component render the entire gameboard
+
+class Board extends React.Component {
+  render() {
+    return(
+      <div className='game-board'>
+        {this.props.gameBoard}
+      </div>
+    )
+  }
+}
+
+function Button(props) {
+  return(
+    <button onClick={props.onClick}>
+      {props.value}
+    </button>
+  )
+}
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props)
+    this.renderBoard = this.renderBoard.bind(this)
+    this.state = {
+      gameBoard: [],
+      showButton: true,
+      height: 8,
+      width: 8
+    }
   }
 
   renderCells(key) {
-    return <Cell key={key}/>
+    return (
+      <Cell 
+        key={key}
+      />
+    )
   }
 
   renderRow(length, key) {
@@ -39,31 +157,35 @@ class Board extends React.Component {
     )
   }
 
-  renderBoard(width, height) {
-    let gameBoard = []
-    for (let i  = 0; i < height; i++) {
-      gameBoard.push(this.renderRow(width, i.toString()))
+  renderBoard() {
+    let gameBoardData = []
+    for (let i  = 0; i < this.state.height; i++) {
+      gameBoardData.push(this.renderRow(this.state.width, i.toString()))
     }
+    
+    this.setState({
+      gameBoard: gameBoardData,
+      showButton: false
+    })
 
-    return gameBoard
+    return gameBoardData
   }
 
-  render() {
-    let gameBoard = this.renderBoard(8,8)
-    console.log(gameBoard)
-    return(
-      <div className='game-board'>
-        {gameBoard}
-      </div>
-    )
-  }
-}
-
-class Game extends React.Component {
   render() {
     return(
       <div className='game'>
-        <Board />
+        <Board 
+          gameBoard = {this.state.gameBoard}
+          width={this.state.width}
+          height={this.state.height}
+        />
+        {this.state.showButton ?
+          <Button 
+            show={this.state.showButton}
+            value='Iniciar juego' 
+            onClick={this.renderBoard}
+          /> 
+          : null}
       </div>
     )
   }
@@ -73,3 +195,7 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 )
+
+// function randomNumber(min, max) {
+//   return Math.floor(Math.random() * (max - min)) + min;
+// }
